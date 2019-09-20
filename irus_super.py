d5eec27b6b6af77073bf73c639d2384a9dc26122
Andrey Zhilyakov <andrey.zhilyakov@apple-2.local>
2019-09-20 17:20:12 -0400

    list comprehntion

--- irus_super.py
+++ irus_super.py
@@ -11,6 +11,24 @@ def find_needle(haystack):
     return "found the needle at position " + str(hey)
 
 
+def move(position, roll):
+    return position + roll * 2
+
+def other_angle(a, b):
+    return 180 - (a+b)   
+
+
+def square_sum(numbers):
+    result = 0
+    for x in numbers:
+        result = result + x * x
+    return result
+
+# def square_sum(numbers):
+    # return sum(x ** 2 for x in numbers)
 
 # print(expression_matter(1,2,3))
 # print(find_needle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']))
+# print(move(3, 6))
+# print(other_angle(30,60))
+print(square_sum([1, 2]))
